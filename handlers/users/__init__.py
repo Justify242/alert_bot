da91b26c8181837f4aef47c9257cd95dfe0653ac
Kirill Valiaev <74653082+Justify242@users.noreply.github.com>
2024-08-12 19:25:57 +0300

    Fix handlers

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,10 +1,10 @@
 from .main_handlers import dp
-from .echo_handlers import dp
 from .inline_handlers import dp
 from .photo_handlers import dp
 from .users_handlers import dp
 from .api_requests_handlers import dp
 from .celery_handlers import dp
+from .echo_handlers import dp
 
 __all__ = ["dp"]
 
